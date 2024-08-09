92f7c70ee94399b8477830d8bac8503eeab2311d
alcileny <mariaalcilenycarvalho@gmail.com>
2024-08-09 16:48:27 -0300

    atualizando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,21 @@ export default function Home() {
     <main >
       <Header/>
       <div className="descrition">
+        <div className="conteudo">
         <h2 className="title"> Transformando ideias em soluções</h2>
         <p className="text">Fábrica Escola de Software</p>
         <p className="descrition-title">Ambiente Pratico e inovador para preparar estudantes para o mercado de Software por meio de projetos reais </p>
         <Button/>
+        </div>
+        <div className="container">
+      <Image
+      src="/mambee.png"
+      width={500}
+      height={500}
+      alt="Picture of the author"
+    />
+
+        </div>
       </div>
     </main>
   );
