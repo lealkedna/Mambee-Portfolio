1d1c599380d3cbd982820ce1138847880506ef38
EmilenyRochaLeal <emilenyleal20@gmail.com>
2024-08-09 15:49:51 -0300

    feat: criação contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,18 +1,27 @@
 import Image from "next/image";
 import Header from "@/compoments/header";
+import style from "@/styles/Contato.module.css";
+import { MdEmail } from "react-icons/md";
 export default function Contato() {
     return (
-        <div>
+        <>
             <Header/>
-            <h1>Sobre</h1>
-            <Image
-                src="/download.jpeg"
-                alt="Vercel Logo"
-                // className="dark:invert"
-                width={100}
-                height={24}
-                priority
-            />
-        </div>
+            <div className={style.section_contato}>
+                <div className={style.container}>
+                        <h1>Fale Com Nosso Cordenador</h1>
+                    <div className={style.info_contato}>
+                        <a href="mailto:mambee.fabrica@gmail.com">
+                                <MdEmail color="#a0ceda"/>
+                            <div>mambee.fabrica@gmail.com</div>
+                        </a>
+                    </div>
+                </div>
+
+                <form>
+                    <input type="text" name="name" placeholder="Nome"></input><br/>
+                    <input type="email" name="email" placeholder="email"></input><br/>
+                </form>            
+            </div>
+        </>
     );
 }
