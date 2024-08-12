9aeb90dc3e1e284b24d365417ec6a6113eac1039
EmilenyRochaLeal <emilenyleal20@gmail.com>
2024-08-12 20:21:23 -0300

    style: implementando favo de mel

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,26 +1,40 @@
 import Image from "next/image";
 import Header from "@/compoments/header";
 import style from "@/styles/Contato.module.css";
+
 import { MdEmail } from "react-icons/md";
+
 export default function Contato() {
     return (
         <>
-            <Header/>
+            <Header />
             <div className={style.section_contato}>
+                <Image className={style.favo_cima}
+                    src="/favoMel.png"
+                    width={160}
+                    height={165}
+                    alt="Favo de mel"
+                />
                 <div className={style.container}>
-                        <h1>Fale Com Nosso Cordenador</h1>
+                    <h1 >Fale Com Nosso Cordenador</h1>
                     <div className={style.info_contato}>
                         <a href="mailto:mambee.fabrica@gmail.com">
-                                <MdEmail color="#a0ceda"/>
+                            <MdEmail color="#a0ceda" />
                             <div>mambee.fabrica@gmail.com</div>
                         </a>
                     </div>
+                <Image className={style.favoMel}
+                    src="/favoMel.png"
+                    width={160}
+                    height={165}
+                    alt="Favo de mel"
+                />
                 </div>
 
                 <form>
-                    <input type="text" name="name" placeholder="Nome"></input><br/>
-                    <input type="email" name="email" placeholder="email"></input><br/>
-                </form>            
+                    <input type="text" name="name" placeholder="Nome"></input><br />
+                    <input type="email" name="email" placeholder="email"></input><br />
+                </form>
             </div>
         </>
     );
