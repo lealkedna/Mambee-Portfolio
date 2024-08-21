418bc703bffacd84b669e8c82446ebdaca99912f
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2024-08-21 10:13:39 -0300

    styles: alterando variaveis de cores

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import styles from "@/styles/Header.module.css"
 function Header() {
     return (
         <header className={styles.header}>
-           <Link  href="/"><h2 className={styles.h2}>Mamb<span className={styles.span}>ee</span></h2></Link>
+           <Link  href="/"><h2 className={styles.h2}>Mambee</h2></Link>
             <nav className={styles.nav}>
                 <ul className={styles.navList}>
                     <li><Link href="/">Início</Link></li>
