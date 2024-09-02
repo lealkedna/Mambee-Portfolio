4ecdc11aafb907ba23138d539cb5945169d6a8b3
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2024-09-02 14:46:55 -0300

    fix: processo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,29 +6,29 @@ import Parceiros from "@/components/parceiros"
 export default function Home() {
   return (
     <main className="principal">
-      <Header/>
+      <Header />
       <div className="descrition">
         <div className="conteudo">
-        <h2 className="title"> Transformando ideias em soluções</h2>
-        <p className="text">Fábrica Escola de Software</p>
-        <p className="descrition-title">Ambiente Pratico e inovador para preparar estudantes para o mercado de Software por meio de projetos reais </p>
-        <Button/>
+          <h2 className="title"> Transformando ideias em soluções</h2>
+          <p className="text">Fábrica Escola de Software</p>
+          <p className="descrition-title">Ambiente Pratico e inovador para preparar estudantes para o mercado de Software por meio de projetos reais </p>
+          <Button />
         </div>
         <div className="container">
-      <Image
-      src="/mambee.png"
-      width={450}
-      height={450}
-      alt="Picture of the author"
-    />
+          <Image
+            src="/mambee.png"
+            width={450}
+            height={450}
+            alt="Picture of the author"
+          />
 
         </div>
       </div>
-      <Parceiros/>
-    <footer>
-      <Footer/>
-    </footer>
-    </main> 
-   
+      <Parceiros />
+      <footer>
+        <Footer />
+      </footer>
+    </main>
+
   );
 }
--- src/components/Processo.tsx
+++ src/components/Processo.tsx
@@ -7,46 +7,48 @@ export default function Processo() {
             <h1 className={styles.textProcess}>Nosso Processo</h1>
             <p className={styles.textParagrafo}>Desenvolvimento Ágil: Inovação, Construção e Avaliação Contínua</p>
 
-            <div className={styles.card}>
-                <Image
-                    src="/mambee.png"
-                    width={450}
-                    height={450}
-                    alt="teste 1"
-                />
-                <h3>Inception</h3>
-                <p>Fase inicial onde as ideias são formadas</p>
-            </div>
+            <div className={styles.cards}>
+                <div className={styles.card}>
+                    <Image
+                        src="/mambee.png"
+                        width={450}
+                        height={450}
+                        alt="teste 1"
+                    />
+                    <h3>Inception</h3>
+                    <p>Fase inicial onde as ideias são formadas</p>
+                </div>
 
-            <div className={styles.card}>
-                <Image
-                    src="/mambee.png"
-                    width={450}
-                    height={450}
-                    alt="teste 1"
-                />
-                <h3>Projeto</h3>
-                <p>Planejamento detalhado</p>
-            </div>
-            <div className={styles.card}>
-                <Image
-                    src="/mambee.png"
-                    width={450}
-                    height={450}
-                    alt="teste 1"
-                />
-                <h3>Implementação</h3>
-                <p>Codificar e desenvolver o projeto</p>
-            </div>
-            <div className={styles.card}>
-                <Image
-                    src="/mambee.png"
-                    width={450}
-                    height={450}
-                    alt="teste 1"
-                />
-                <h3>Avaliação</h3>
-                <p>Revisão final do trabalho</p>
+                <div className={styles.card}>
+                    <Image
+                        src="/mambee.png"
+                        width={450}
+                        height={450}
+                        alt="teste 1"
+                    />
+                    <h3>Projeto</h3>
+                    <p>Planejamento detalhado</p>
+                </div>
+                <div className={styles.card}>
+                    <Image
+                        src="/mambee.png"
+                        width={450}
+                        height={450}
+                        alt="teste 1"
+                    />
+                    <h3>Implementação</h3>
+                    <p>Codificar e desenvolver o projeto</p>
+                </div>
+                <div className={styles.card}>
+                    <Image
+                        src="/mambee.png"
+                        width={450}
+                        height={450}
+                        alt="teste 1"
+                    />
+                    <h3>Avaliação</h3>
+                    <p>Revisão final do trabalho</p>
+                </div>
             </div>
 
         </div>
