e227dc1ac6d26f6b80c051224c0a8d56c20b9d25
alcileny <mariaalcilenycarvalho@gmail.com>
2024-09-04 11:01:39 -0300

    organização

--- src/components/Parceiros.tsx
+++ src/components/Parceiros.tsx
@@ -3,54 +3,56 @@ import styles from "@/styles/Parceiros.module.css";
 export default function Parceriros(){
     return (
     <section className={styles.parceiros}>
-    <h2 className={styles.title}>Parceiros</h2>
+    <h2 className={styles.title}> Parceiros</h2>
     <div >
       <div className={styles.coluna1}>
         <Image
-          src="/images/ifpiPicos.png"
-          width={400}
-          height={100}
+          src="/images/labitec.png"
+          width={110}
+          height={110}
           alt="Parceiro 1"
         />
         <Image
-          src="/images/valedomel.png"
-          width={400}
+          src="/images/pete.png"
+          width={200}
           height={100}
           alt="Parceiro 2"
         />
          <Image
-          src="/images/ibict.png"
-          width={400}
+          src="/images/mb.png"
+          width={150}
           height={100}
           alt="Parceiro 3"
         />
-         <Image
-          src="/images/mb.png"
-          width={400}
+        
+        
+      </div>
+      <div className={styles.coluna2}>
+      <Image
+          src="/images/ifpiPicos.png"
+          width={300}
           height={100}
           alt="Parceiro 4"
         />
-      </div>
-      <div className={styles.coluna2}>
         <Image
-          src="/images/mb.png"
-          width={200}
+          src="/images/valedomel.png"
+          width={400}
           height={80}
           alt="Parceiro 5"
         />
         <Image
           src="/images/virtex.png"
-          width={200}
-          height={80}
+          width={250}
+          height={100}
           alt="Parceiro 6"
         />
          <Image
-          src="/images/labitec.png"
-          width={200}
-          height={80}
+          src="/images/ibict.png"
+          width={300}
+          height={100}
           alt="Parceiro 6"
         />
-        
+       
       </div>
       
     </div>
